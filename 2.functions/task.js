328b56d3d0ec1a5ea061e0905278092f205b1a9e
Юлия Фёдорова <uliafedorova297@gmail.com>
2023-03-14 20:32:52 +0300

    2,4,6,7,8 дз сделанны

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,74 @@
+"use strict";
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+  if (arr.length == 0) {
+    return 0;
+  }
+  let summa = (sum, elem) => sum + elem;
+  let avg = arr.reduce(summa) / arr.length;
+  return {
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    avg: +avg.toFixed(2),
+  };
 }
 
 function summElementsWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  let summa = (sum, elem) => sum + elem;
+  return arr.reduce(summa);
 }
 
 function differenceMaxMinWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  return Math.max.apply(null, arr) - Math.min.apply(null, arr);
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length == 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = func(...arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i++) {
+    const funcResult = func(...arrOfArr[i]);
+    if (funcResult > maxWorkerResult) {
+      maxWorkerResult = funcResult;
+    }
+  }
+  return maxWorkerResult;
 }
+
+/*function makeWork(arrOfArr, func) {
+return Math.max(...arrOfArr.map((arr) => func(arr)));
+}*/
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
+"use strict";
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+  this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
+};
 
 Student.prototype.getAverage = function () {
-  
-}
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  } else {
+    return (
+      this.marks.reduce((accum, current) => accum + current, 0) /
+      this.marks.length
+    );
+  }
+};
 
 Student.prototype.exclude = function (reason) {
-  
-}
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
